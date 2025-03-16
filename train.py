caf2d06c361ea6eef6bc4be9e9c875dae1709d6b
Sophia Zhang <sophia.p.zhang@gmail.com>
2025-03-16 06:02:58 -0700

    epochs

--- train.py
+++ train.py
@@ -94,7 +94,7 @@ def train(train = False):
         device = "cuda" if torch.cuda.is_available() else "cpu"
 
         # hyperparameters
-        num_epochs = 10
+        num_epochs = 5
         lambda_cycle = 5
         learning_rate = 1e-4
         batch_size = 1
