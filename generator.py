d833b18d01c590f6826f1cadae5cb4a54966d73f
Sophia Zhang <sophia.p.zhang@gmail.com>
2025-03-16 05:36:34 -0700

    new generator

--- generator.py
+++ generator.py
@@ -1,21 +1,49 @@
-# small generator model for GAN
-
 import torch
 import torch.nn as nn
 
+class ConvBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, down=True, use_act=True, **kwargs):
+        super().__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, padding_mode="reflect", **kwargs)
+            if down
+            else nn.ConvTranspose2d(in_channels, out_channels, **kwargs),
+            nn.InstanceNorm2d(out_channels),
+            nn.ReLU(inplace=True) if use_act else nn.Identity(),
+        )
+
+    def forward(self, x):
+        return self.conv(x)
+
+
+class ResidualBlock(nn.Module):
+    def __init__(self, channels):
+        super().__init__()
+        self.block = nn.Sequential(
+            ConvBlock(channels, channels, kernel_size=3, padding=1),
+            ConvBlock(channels, channels, use_act=False, kernel_size=3, padding=1),
+        )
+
+    def forward(self, x):
+        return x + self.block(x)
+
+
 class Generator(nn.Module):
-    def __init__(self, img_channels=3, num_features=32):
+    def __init__(self, img_channels=3, num_features=64, num_residuals=4):
         super().__init__()
         self.initial = nn.Sequential(
             nn.Conv2d(img_channels, num_features, kernel_size=7, stride=1, padding=3, padding_mode="reflect"),
+            nn.InstanceNorm2d(num_features),
             nn.ReLU(inplace=True),
         )
+        
+        self.down1 = ConvBlock(num_features, num_features * 2, kernel_size=3, stride=2, padding=1)
+        self.down2 = ConvBlock(num_features * 2, num_features * 4, kernel_size=3, stride=2, padding=1)
 
-        self.down1 = nn.Conv2d(num_features, num_features * 2, kernel_size=3, stride=2, padding=1)
-        self.down2 = nn.Conv2d(num_features * 2, num_features * 4, kernel_size=3, stride=2, padding=1)
+        self.res_blocks = nn.Sequential(*[ResidualBlock(num_features * 4) for _ in range(num_residuals)])
 
-        self.up1 = nn.ConvTranspose2d(num_features * 4, num_features * 2, kernel_size=3, stride=2, padding=1, output_padding=1)
-        self.up2 = nn.ConvTranspose2d(num_features * 2, num_features, kernel_size=3, stride=2, padding=1, output_padding=1)
+        self.up1 = ConvBlock(num_features * 4, num_features * 2, down=False, kernel_size=3, stride=2, padding=1, output_padding=1)
+        self.up2 = ConvBlock(num_features * 2, num_features, down=False, kernel_size=3, stride=2, padding=1, output_padding=1)
 
         self.final = nn.Conv2d(num_features, img_channels, kernel_size=7, stride=1, padding=3, padding_mode="reflect")
 
@@ -23,8 +51,7 @@ class Generator(nn.Module):
         x = self.initial(x)
         x = self.down1(x)
         x = self.down2(x)
+        x = self.res_blocks(x)
         x = self.up1(x)
         x = self.up2(x)
         return torch.tanh(self.final(x))
-
-
